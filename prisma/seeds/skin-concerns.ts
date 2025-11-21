b38656033dbff83df7db157fc85fae0f247782c2
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2025-11-21 23:16:13 +0700

    feat: add seedSkinConcerns function for populating skin concerns in the database

---
+++ prisma/seeds/skin-concerns.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from '@prisma/client'
+
+export const seedSkinConcerns = async (prisma: PrismaClient) => {
+    const concerns = [
+        'Acne-prone',
+        'Sensitive',
+        'Uneven tone',
+        'Cracking',
+        'Dark spots',
+        'Dullness',
+        'Large pores',
+        'Wrinkle/Aging',
+        'Dark circles',
+        'Hyperpigmentation',
+    ]
+
+    console.log('running seed: skin-concerns...')
+
+    await prisma.skinConcern.createMany({
+        data: concerns.map((name) => ({ name })),
+        skipDuplicates: true,
+    })
+}
